0fa5de5a292be475a2f8c60ba4c1a17d432cf6e0
Jebin1991s <jebin1991s@gmail.com>
2026-04-14 15:23:58 +0530

    Update main.tf

--- terraform-code/simple-server-creation/main.tf
+++ terraform-code/simple-server-creation/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-server" {
   ami           = "ami-026b57f3c383c2eec"
-  instance_type = "t2.micro"
-  key_name      = "night"
+  instance_type = "m7i-flex.large"
+  key_name      = "Devops-Test"
   tags = {
     Name = "This server by manual"
   }
